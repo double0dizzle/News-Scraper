c553cac2566cd70f088b077249a52d8f14a8bf6f
Kristin Herrmann <dickmann.michael@gmail.com>
2018-09-27 21:07:32 -0600

    models folder

--- server.js
+++ server.js
@@ -66,6 +66,18 @@ app.get('/scrape', function (req, res, next) {
 //           // If an error occurred, send it to the client
 //           return res.json(err);
 //         });
+app.get("/articles", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+      .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
 
 
 app.get('/contact', function(req, res, next){
