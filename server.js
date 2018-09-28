c24571ba999d5c820978dc223aec046f0b45ed32
Kristin Herrmann <dickmann.michael@gmail.com>
2018-09-28 16:32:48 -0600

    rendering scrape

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require("express");
 // var mongojs = require("mongojs");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-
+var port = process.env.PORT || 3000;
 // var $ = cheerio.load(html);
 var app = express();
 
@@ -15,9 +15,26 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+var db = require("./models")
+
 app.get('/', function(req, res, next){
-    res.render('index');
+    res.redirect('/articles');
 });
+
+// app.get('/contact', function(req, res, next){
+//     db.Article.find({})
+//       .then(function(dbArticle) {
+//         // If we were able to successfully find Articles, send them back to the client
+//         // res.json(dbArticle);
+//         res.render('index', {articles: dbArticle});
+//         // res.render('index');
+//       })
+//       .catch(function(err) {
+//         // If an error occurred, send it to the client
+//         res.json(err);
+//       });
+    
+// });
 
 app.get('/scrape', function (req, res, next) {
     console.log("\n***********************************\n" +
@@ -31,29 +48,37 @@ app.get('/scrape', function (req, res, next) {
         var $ = cheerio.load(html);
 
         // An empty array to save the data that we'll scrape
-        var results = [];
+        
 
         // With cheerio, find each p-tag with the "title" class
         // (i: iterator. element: the current element)
         $("p.title").each(function (i, element) {
-
+            var results = {};
             // Save the text of the element in a "title" variable
-            var title = $(element).text();
+            results.title = $(element).text();
 
             // In the currently selected element, look at its child elements (i.e., its a-tags),
             // then save the values for any "href" attributes that the child elements may have
-            var link = $(element).children().attr("href");
+            results.link = $(element).children().attr("href");
 
             // Save these results in an object that we'll push into the results array we defined earlier
-            results.push({
-                title: title,
-                link: link
+            db.Article.create(results)
+            .then(function(dbArticle) {
+              // View the added result in the console
+              console.log(dbArticle);
+            })
+            .catch(function(err) {
+              // If an error occurred, send it to the client
+              return res.json(err);
+              
             });
+            console.log(results);
         });
+        
 
         // Log the results once you've looped through each of the elements found with cheerio
-        console.log(results);
-        res.json(results);
+        
+        res.send('scrape complete');
     });
 });
 
@@ -66,16 +91,25 @@ app.get('/scrape', function (req, res, next) {
 //           // If an error occurred, send it to the client
 //           return res.json(err);
 //         });
+var mongoURL = process.env.MONGODB_URI || "mongodb://localhost/scrape"
+mongoose.connect(mongoURL, { useNewUrlParser: true }, function(){
+    console.log("Connection Successful")
+});
+
+
 app.get("/articles", function(req, res) {
     // Grab every document in the Articles collection
     db.Article.find({})
       .then(function(dbArticle) {
         // If we were able to successfully find Articles, send them back to the client
-        res.json(dbArticle);
+        // res.json(dbArticle);
+        console.log(dbArticle[0].title)
+        res.render('articles', {articles: dbArticle});
       })
       .catch(function(err) {
         // If an error occurred, send it to the client
         res.json(err);
+        
       });
   });
 
@@ -84,6 +118,6 @@ app.get('/contact', function(req, res, next){
     res.render('contact');
 });
 
-app.listen(3000, function() {
+app.listen(port, function() {
     console.log('app listening on port 3000')
 })
